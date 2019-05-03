7d6ad2bd0931a44b2a76f89c0ec48d53b6653f04
Fredy Sanchez <00082817@uca.edu.sv>
2019-05-03 10:19:02 -0600

    No hace nada

---
+++ app/src/main/java/com/numerica98/evlab2/botones_fragment.kt
@@ -0,0 +1,30 @@
+package com.numerica98.evlab2
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.botones_fragment.*
+
+class botones_fragment : Fragment() {
+
+    lateinit var botonIzq : Button
+    lateinit var botonDer : Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater.inflate(R.layout.botones_fragment, container, false)
+        with(this){
+            botonIzq = boton_izq
+            botonDer = boton_der
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/numerica98/evlab2/imagen_fragment.kt
@@ -0,0 +1,34 @@
+package com.numerica98.evlab2
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.imagen_fragment.*
+import kotlin.random.Random
+
+class imagen_fragment : Fragment() {
+
+    lateinit var picture : ImageView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var imgNum : Int = 2
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.imagen_fragment, container, false)
+        with(this){
+            picture = image
+        }
+
+        picture.setImageResource(
+            when(imgNum){
+                2 -> R.drawable.img1
+                3 -> R.drawable.img3
+                else -> R.drawable.img1
+            }
+        )
+        return view;
+
+    }
+}
